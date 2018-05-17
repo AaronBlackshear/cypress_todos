a7a6ecbf7b56f25ff5dc62d74aa5dca6e4c5003a
Aaron Blackshear <theshiftybapple@gmail.com>
2018-05-17 12:32:54 -0500

    Cypress tests

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import "./App.css";
 
@@ -13,26 +14,46 @@ class App extends Component {
       //   isComplete: false
       // }
     ],
-    inputText: ""
+    inputText: "",
+    errorMsg: ""
   };
+
+  componentDidMount() {
+    axios.get("/api/todos").then(res => {
+      this.setState({ todos: res.data });
+    });
+  }
 
   handleChange = e => {
     this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("/api/todos", { title: this.state.inputText })
+      .then(res => this.setState({ todos: res.data, inputText: "" }))
+      .catch(err => this.setState({ errorMsg: "Not Happening" }));
   };
 
   render() {
     return (
       <div className="App">
         <header className="App-header" />
-        <input
-          data-cypress-input
-          value={this.state.inputText}
-          name="inputText"
-          onChange={this.handleChange}
-          autoFocus
-          className="new_todo"
-          placeholder="Add new Todo"
-        />
+        {this.state.errorMsg && (
+          <span className="error">{this.state.errorMsg}</span>
+        )}
+        <form onSubmit={this.handleSubmit} action="">
+          <input
+            data-cypress-input
+            value={this.state.inputText}
+            name="inputText"
+            onChange={this.handleChange}
+            autoFocus
+            className="new_todo"
+            placeholder="Add new Todo"
+          />
+        </form>
         <List todos={this.state.todos} />
       </div>
     );
